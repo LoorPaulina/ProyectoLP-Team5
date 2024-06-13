7e86c70c98679c71bb37fa587cae40be1c754106
TaizRuiz <dafnetaizruiz@gmail.com>
2024-06-13 13:53:53 -0500

    regex ruiz

--- lexico.py
+++ lexico.py
@@ -1,13 +1,29 @@
 import ply.lex as lex
 #lista de nombres de tokens 
 tokens=(
-    'NUMERO',
+    'IGUAL',
+    'ENTERO',
     'MAS',
     'MENOS',
     'DIVISION',
-    'PARENTESIS_L'
-    'PARENTESIS_R'
+    'PARENTESIS_L',
+    'PARENTESIS_R',
+    'VARIABLE',
+    'FLOTANTE',
+    'CADENA'
+    
+    
 )
 
 #expresiones regulares para los tokens 
-t_NUMERO= r'^[-]?[0-9]*$'
+t_IGUAL=r'='
+t_ENTERO= r'^[-]?[0-9]*$'
+t_MAS= r'\+'
+t_MAS= r'-'
+t_DIVISION= r'/'
+t_PARENTESIS_L= r'('
+t_PARENTESIS_R= r')'
+t_VARIABLE=r'^[$@]?[a-zA-Z]{1,}\w*$'
+t_FLOTANTE=r'^[-]?(0|[1-9]\d*)?\.{1}\d*$'
+t_CADENA=r'^"[^"]*"$'
+
